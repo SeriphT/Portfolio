fff40ddbea54617779fdcd6e9b5f72e000f9ca29
SeriphT <42582157+SeriphT@users.noreply.github.com>
2019-01-04 09:27:20 -0700

    Add files via upload

--- Trivia/T2 final quiz.py
+++ Trivia/T2 final quiz.py
@@ -21,7 +21,47 @@ def next_line(trivia):
     line = line.replace("/","\n")
     return line
 
-file_name = "test_questions.txt"
-trivia = open_file(file_name,"r")
-line = next_line(trivia)
-print(line)
+def next_block(trivia):
+    category = next_line(trivia)
+    question = next_line(trivia)
+    answers = []
+    for i in range(4):
+        ans = next_line(trivia)
+        answers.append(ans)
+    correct = next_line(trivia)
+    if correct:
+        correct = correct[0]
+    explanation = next_line(trivia)
+    return category, question, answers, correct, explanation
+
+def welcome(title):
+    print("\t\tWelcome to Trivia Challenge!")
+    print('\t\t',title,'\t\t')
+
+def main():
+    trivia = open_file("test_questions.txt", "r")
+    title = next_line(trivia)
+    welcome(title)
+    score = 0
+    category, question, answers, correct, explanation = next_block(trivia)
+    while category:
+        print(category)
+        x =0
+        for i in range(4):
+            x+=1
+            print(x, answers[i])
+        answer = input("Your Answer:")
+        if correct == answer:
+                print("Correct!")
+                score += 10
+        else:
+                print("Incorrect!")
+        print(explanation)
+        print(score)
+        next_block(trivia)
+    print(score)
+    trivia.close()
+    input("Press enter to exit the program.")
+    sys.exit()
+
+main()
